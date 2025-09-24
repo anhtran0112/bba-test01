734b8e074204fd63879e2b7532b6364a67b15d35
duyanh <duyanh011286@gmail.com>
2025-09-24 22:56:01 +0700

    feat: add solution for challenge 03

--- tests/lession-04/cloud-kingdom.js
+++ tests/lession-04/cloud-kingdom.js
@@ -4,13 +4,8 @@ const level1Coins = 25;
 const level2Coins = 30;
 const level3Coins = 45;
 
-
 function divideWithRemainder(a, b, c) {
-
     return (a + b + c) % currentLives;
-
 }
-
-const remainder = divideWithRemainder (level1Coins,level2Coins,level3Coins);
-// In ra số coin dư khi chia cho 3
+const remainder = divideWithRemainder(level1Coins, level2Coins, level3Coins);
 console.log(`Số coin dư khi chia tổng coin cho 3: ${remainder}`);
